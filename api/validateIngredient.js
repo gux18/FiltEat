c7e866c92165272be806d917c780a1ee6447cd14
Hello <108929116+Kopeer@users.noreply.github.com>
2026-07-30 12:58:01 +0900

    Update validateIngredient.js

--- api/validateIngredient.js
+++ api/validateIngredient.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
   try {
     const text = req.body.text.trim().replace(/\s+/g," ");
     
-    if (/^[A-Za-z]+$/.test(text)) {
+    if (/[A-Za-z]/.test(text)) {
       return res.status(200).json({ valid:false });
     }
 
