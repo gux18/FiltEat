4d7aa1ba84191c2201cd5812ab914fd95767e1b4
Hello <108929116+Kopeer@users.noreply.github.com>
2026-07-30 13:03:03 +0900

    Update validateIngredient.js

--- api/validateIngredient.js
+++ api/validateIngredient.js
@@ -6,6 +6,10 @@ const ai = new GoogleGenAI({apiKey: process.env.GEMINI_API_KEY});
 export default async function handler(req, res) {
   try {
     const text = req.body.text.trim().replace(/\s+/g," ");
+
+    if (text.length > 25) {
+      return res.status(200).json({valid:false});
+    }
     
     if (/[A-Za-z]/.test(text)) {
       return res.status(200).json({ valid:false });
